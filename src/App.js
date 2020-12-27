37451c39773a1ebc09b7c2fcccd22f1cd85aefe7
iamtreetop <trivta@gmail.com>
2020-12-27 14:45:40 -0800

    Add collections to database

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import CheckoutPage from "./pages/checkout/checkout.component";
 import Header from "./components/header/header.component";
 
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+
 import { setCurrentUser } from './redux/user/user.actions'
 import { selectCurrentUser } from "./redux/user/user.selectors";
 
@@ -33,10 +34,9 @@ class App extends React.Component {
               ...snapShot.data()
           });
         });
-
-      } else {
-        setCurrentUser(userAuth);
       }
+      setCurrentUser(userAuth);
+      
     });
   }
 
@@ -71,7 +71,7 @@ class App extends React.Component {
 }
 
 const mapSTP = createStructuredSelector({
-  currentUser: selectCurrentUser
+  currentUser: selectCurrentUser,
 });
 // const mapSTP = (state) => {
 //     return ({
