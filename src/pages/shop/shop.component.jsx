2dbba482bcfbc38967d3b5b8c81cb5c5d9500dea
iamtreetop <trivta@gmail.com>
2020-12-28 19:47:13 -0800

    Move async actionConvert async shop actions to saga actions

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -3,7 +3,7 @@ import { Route } from 'react-router-dom';
 // import { createStructuredSelector } from 'reselect';
 import { connect } from 'react-redux';
 
-import { fetchCollectionsStartAsync } from '../../redux/shop/shop.actions';
+import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
 
 // import WithSpinner from '../../components/with-spinner/with-spinner.component'
 
@@ -17,9 +17,9 @@ import CollectionPageContainer from '../collection/collection.container';
 
 class ShopPage extends React.Component {
   componentDidMount(){
-    const { fetchCollectionsStartAsync } = this.props;
+    const { fetchCollectionsStart } = this.props;
 
-    fetchCollectionsStartAsync();
+    fetchCollectionsStart();
   };
 
   render() {
@@ -48,7 +48,7 @@ class ShopPage extends React.Component {
 // });
 
 const mapDTP = dispatch => ({
-  fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync())
+  fetchCollectionsStart: () => dispatch(fetchCollectionsStart())
 });
 
 export default connect(
---
+++ src/redux/shop/shop.sagas.js
@@ -0,0 +1,36 @@
+import { takeEvery, call, put } from 'redux-saga/effects';
+
+import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils'
+
+import {
+  fetchCollectionsFailure,
+  fetchCollectionsSuccess,
+  FETCH_COLLECTIONS_START
+} from './shop.actions';
+
+export function* fetchCollectionsAsync() {
+  yield console.log("i am fired");
+
+  try {
+    const collectionRef = firestore.collection('collections');
+    const snapshot = yield collectionRef.get();
+    const collectionsMap = yield call(convertCollectionsSnapshotToMap, snapshot)
+    yield put(fetchCollectionsSuccess(collectionsMap));
+  } catch (error) {
+    yield put(fetchCollectionsFailure(error.message));
+  }
+    // collectionRef
+    //   .get()
+    //   .then(snapshot => {
+    //     const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+    //     dispatch(fetchCollectionsSuccess(collectionsMap));
+    //   })
+    //   .catch(error => dispatch(fetchCollectionsFailure(error.message)));
+}
+
+export function* fetchCollectionsStart() {
+  yield takeEvery(
+    FETCH_COLLECTIONS_START,
+    fetchCollectionsAsync 
+  );
+};
