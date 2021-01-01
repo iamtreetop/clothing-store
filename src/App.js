743e673a00560ac27f652b7d787b4fa1f97406d3
iamtreetop <trivta@gmail.com>
2021-01-01 12:16:35 -0800

    Convert App.js and ShopPage to functional components, add useEffect to both components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route, Switch, Redirect } from "react-router-dom";
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
@@ -18,44 +18,36 @@ import Header from "./components/header/header.component";
 import { selectCurrentUser } from "./redux/user/user.selectors";
 import { checkUserSession } from './redux/user/user.actions';
 
-class App extends React.Component {
+// class App extends React.Component {
+const App = ({ checkUserSession, currentUser }) => {
+  useEffect(() => {
+    checkUserSession()
+  }, [checkUserSession]);
 
-  unsubscribeFromAuth = null;
-
-  componentDidMount = async() => {
-    const { checkUserSession } = this.props;
-    checkUserSession();
-  }
-
-
-  componentWillUnmount() {
-    this.unsubscribeFromAuth();
-  }
-
-  render() {
-    return (
-      <div>
-        <Header />
-        <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route path='/shop' component={ShopPage} />
-          <Route eaxt path='/checkout' component={CheckoutPage} />
-          <Route
-            exact
-            path='/signin'
-            render={() =>
-              this.props.currentUser ? (
-                <Redirect to='/' />
-              ) : (
-                <SignInAndSignUpPage />
-              )
-            }
-          />
-        </Switch>
-      </div>
-    );
-  }
-}
+  // render() {
+  return (
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+        <Route eaxt path='/checkout' component={CheckoutPage} />
+        <Route
+          exact
+          path='/signin'
+          render={() =>
+            currentUser ? (
+              <Redirect to='/' />
+            ) : (
+              <SignInAndSignUpPage />
+            )
+          }
+        />
+      </Switch>
+    </div>
+  );
+  // }
+};
 
 const mapSTP = createStructuredSelector({
   currentUser: selectCurrentUser,
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route } from 'react-router-dom';
 // import { createStructuredSelector } from 'reselect';
 import { connect } from 'react-redux';
@@ -15,31 +15,27 @@ import CollectionPageContainer from '../collection/collection.container';
 // const CollectionPageWithSpinner = WithSpinner(CollectionPage);
 
 
-class ShopPage extends React.Component {
-  componentDidMount(){
-    const { fetchCollectionsStart } = this.props;
+// class ShopPage extends React.Component {
+const ShopPage = ({ fetchCollectionsStart, match }) => {
 
-    fetchCollectionsStart();
-  };
+  useEffect(() => {
+    fetchCollectionsStart()
+  }, [fetchCollectionsStart])
 
-  render() {
-    const { match } = this.props;
-
-    return (
-      <div className='shop-page'>
-        <Route 
-          exact path={`${match.path}`} 
-          component={CollectionsOverviewContainer}
-          // render={(props) => <CollectionOverviewWithSpinner isLoading={!isFetchingCollections} {...props} />} 
-        />
-        <Route 
-          path={`${match.path}/:collectionId`} 
-          // render={(props) => <CollectionPageWithSpinner isLoading={!isCollectionloaded} {...props} />}
-          component={CollectionPageContainer}
-        />
-      </div>
-    );
-  }
+  return (
+    <div className='shop-page'>
+      <Route 
+        exact path={`${match.path}`} 
+        component={CollectionsOverviewContainer}
+        // render={(props) => <CollectionOverviewWithSpinner isLoading={!isFetchingCollections} {...props} />} 
+      />
+      <Route 
+        path={`${match.path}/:collectionId`} 
+        // render={(props) => <CollectionPageWithSpinner isLoading={!isCollectionloaded} {...props} />}
+        component={CollectionPageContainer}
+      />
+    </div>
+  );
 };
 
 // const mapSTP = createStructuredSelector({
