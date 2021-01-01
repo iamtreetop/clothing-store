55574e7c3daf261f694322930a91aeab0feee363
iamtreetop <trivta@gmail.com>
2020-12-31 18:28:05 -0800

    Convert sign in component to funcitonal componeent using hooks

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 
 import FormInput from "../form-input/form-input.component";
@@ -12,59 +12,58 @@ import {
 
 import './sign-in.styles.scss'
 
-class SignIn extends React.Component {
-  constructor() {
-    super();
+// class SignIn extends React.Component {
+  // constructor() {
+  //   super();
 
-    this.state = {
-      email: '',
-      password: ''
-    };
-  };
-
-  handleSubmit = async (e) => {
-    e.preventDefault();
-    // debugger
-    const { emailSignInStart } = this.props;
-    const { email, password } = this.state;
-
-  //   try {
-  //     await auth.signInWithEmailAndPassword(email, password);
-  //     this.setState({ email: '', password: ''})
-  //   } catch (error) {
-  //     console.log(error);
+  //   this.state = {
+  //     email: '',
+  //     password: ''
   //   };
+  // };
+  // const { emailSignInStart } = this.props;
+  // const { email, password } = this.state;
+const SignIn = ({ emailSignInStart, googleSignInStart }) => {
+  const [userCredentials, setCredentials] = useState({ 
+    email: '', 
+    password: '' 
+  });
+  const { email, password } = userCredentials;
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
     emailSignInStart(email, password);
   };
 
-  handleChange = (e) => {
+  const handleChange = (e) => {
     const {value, name} = e.target;
 
-    this.setState({ [name]: value})
+    // this.setState({ [name]: value})
+    setCredentials({...userCredentials, [name]: value})
   };
 
-  render() {
+  // render() {
     // debugger
-    const { googleSignInStart } = this.props;
+    // const { googleSignInStart } = this.props;
     return (
       <div className='sign-in'>
         <h2>I already have an account</h2>
         <span>Sign in with your email and password</span>
 
-        <form onSubmit={this.handleSubmit}>
+        <form onSubmit={handleSubmit}>
           <FormInput 
             type='email' 
             name='email' 
-            value={this.state.email} 
-            handleChange={this.handleChange}
+            value={email} 
+            handleChange={handleChange}
             label='email'
             required />
           <FormInput 
             type='password' 
             name='password' 
-            value={this.state.password} 
-            handleChange={this.handleChange}
+            value={password} 
+            handleChange={handleChange}
             label='password'
             required />
           <div className='buttons'>
@@ -80,7 +79,7 @@ class SignIn extends React.Component {
         </form>
       </div>
     )
-  };
+  // };
 };
 
 const mapDTP = (dispatch) => {
