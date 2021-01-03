f83eeeee6c070efbe8a624583b592385d6ad5d73
iamtreetop <trivta@gmail.com>
2021-01-02 16:08:03 -0800

    Update CSS for header and menu item to make compoanents mobile responsive

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,13 @@ import { Route, Switch, Redirect } from "react-router-dom";
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import './App.css';
-
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import CheckoutPage from "./pages/checkout/checkout.component";
-
 import Header from "./components/header/header.component";
 
+import { GlobalStyle } from './global.styles';
 // import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 // import { setCurrentUser } from './redux/user/user.actions'
@@ -27,6 +25,7 @@ const App = ({ checkUserSession, currentUser }) => {
   // render() {
   return (
     <div>
+      <GlobalStyle />
       <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
